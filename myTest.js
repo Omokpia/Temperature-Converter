c936b4685941cd1c14c6fe4c65dd17942e10c740
Godspower Omokpia <omokpia59@gmail.com>
2025-08-22 22:35:47 +0100

    Update myTest.js

--- myTest.js
+++ myTest.js
@@ -1,3 +1,57 @@
-// Write a temperature converter application that converts temperatures
-//  between Celsius, kelvin and Fahrenheit.
+// Write a temperature converter application that converts temperature 
+// between Celsius, kelvin and Fahrenheit.
+
+
+function convertCelsiusToKelvin(temp) {
+    let newTemp = temp + 273.15
+
+    console.log('This is the new converted celsius temperature to Kelvin : ', newTemp);
+}
+
+
+function convertCelsiusToFahrenheit(temp) {
+    let newTemp = (temp * 9/5) + 32
+
+    console.log('This is the new converted celsius temperature to Fahrenheit : ', newTemp);
+}
+
+function convertKelvinToCelsius(temp) {
+    let newTemp = temp - 273.15
+
+    console.log('This is the new converted kelvin temperature to Celsius : ', newTemp);
+}
+
+function convertKelvinToFahrenheit(temp) {
+    let newTemp = (temp - 273.15) * 9/5 + 32
+
+    console.log('This is the new converted kelvin temperature to Fahrenheit : ', newTemp);
+}
+
+function convertFahrenheitToCelsius(temp) {
+    let newTemp = (temp - 32) * 5/9
+
+    console.log('This is the new converted fahrenheit temperature to Celsius : ', newTemp);
+}
+
+function convertFahrenheitToKelvin(temp) {
+    let newTemp = (temp - 32) * 5/9 + 273.15
+
+    console.log('This is the new converted fahrenheit temperature to Kelvin : ', newTemp);
+}
+convertCelsiusToKelvin()
+convertCelsiusToFahrenheit()  
+convertKelvinToCelsius()
+convertKelvinToFahrenheit()
+convertFahrenheitToCelsius()
+convertFahrenheitToKelvin()
+
+
+
+
+
+
+
+
+
+
 
